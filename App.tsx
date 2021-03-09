70869afab6afc531507d1e7d06606f916f1e1e1f
Navas <gabrielnavas@email.com>
2021-03-09 02:26:59 -0300

    learning expo camera react native

--- App.tsx
+++ App.tsx
@@ -1,16 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import { Camera } from 'expo-camera';
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(false);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [camera, setCamera] = useState<Camera>();
+  const [imageUrl, setImageUrl] = useState<undefined | string>()
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Camera 
+        ref={ref => {
+          if(ref) setCamera(ref);
+        }}
+        style={styles.camera} 
+        type={type}
+        flashMode={Camera.Constants.FlashMode}
+      >
+        <View style={styles.button}>
+          <TouchableOpacity
+            style={styles.buttonInside}
+            onPress={() => {
+              // setType(
+              //   type === Camera.Constants.Type.back
+              //     ? Camera.Constants.Type.front
+              //     : Camera.Constants.Type.back
+              // );
+              camera?.takePictureAsync({
+                onPictureSaved: picture => {
+                  console.log(picture)
+                  setImageUrl(picture.uri)
+                },
+
+              })
+            }}>
+            <Text style={{color: 'red'}}> Flip </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+      {
+        imageUrl && (
+          <Image
+            source={{ uri: imageUrl }}
+            style={styles.image}
+          />
+        )
+      }
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -18,4 +70,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  camera: {
+    width: '100%',
+    height: '80%',
+    justifyContent: 'center',
+    alignItems: 'flex-end', 
+  },
+  button: {
+    backgroundColor: 'red', 
+    width: 50, 
+    height: 50,
+  },
+  buttonInside: {
+    backgroundColor: 'green',
+    width: 50, 
+    height: 50, 
+  },
+  image: {
+    width: 50,
+    height: 50,
+  }
 });
